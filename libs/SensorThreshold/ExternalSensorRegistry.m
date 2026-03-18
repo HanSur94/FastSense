177f885e9a0d8dbb584be7c799ce90011f4b6e69
Hannes Suhr <sannahrush@googlemail.com>
2026-03-18 22:43:49 +0100

    feat: add register/get/unregister/getMultiple/getAll to ExternalSensorRegistry

--- libs/SensorThreshold/ExternalSensorRegistry.m
+++ libs/SensorThreshold/ExternalSensorRegistry.m
@@ -36,5 +36,43 @@ classdef ExternalSensorRegistry < handle
             %KEYS Return all registered sensor keys.
             k = obj.catalog_.keys();
         end
+
+        function register(obj, key, sensor)
+            %REGISTER Add a Sensor to the catalog.
+            %   reg.register('key', sensorObj)
+            assert(isa(sensor, 'Sensor'), ...
+                'ExternalSensorRegistry:invalidType', ...
+                'Value must be a Sensor object.');
+            obj.catalog_(key) = sensor;
+        end
+
+        function unregister(obj, key)
+            %UNREGISTER Remove a Sensor from the catalog.
+            if obj.catalog_.isKey(key)
+                obj.catalog_.remove(key);
+            end
+        end
+
+        function s = get(obj, key)
+            %GET Retrieve a sensor by key.
+            if ~obj.catalog_.isKey(key)
+                error('ExternalSensorRegistry:unknownKey', ...
+                    'No sensor with key ''%s'' in registry ''%s''.', key, obj.Name);
+            end
+            s = obj.catalog_(key);
+        end
+
+        function sensors = getMultiple(obj, keys)
+            %GETMULTIPLE Retrieve multiple sensors by key.
+            sensors = cell(1, numel(keys));
+            for i = 1:numel(keys)
+                sensors{i} = obj.get(keys{i});
+            end
+        end
+
+        function m = getAll(obj)
+            %GETALL Return a copy of the catalog as a containers.Map.
+            m = containers.Map(obj.catalog_.keys(), obj.catalog_.values());
+        end
     end
 end
--- tests/suite/TestExternalSensorRegistry.m
+++ tests/suite/TestExternalSensorRegistry.m
@@ -17,5 +17,77 @@ classdef TestExternalSensorRegistry < matlab.unittest.TestCase
             testCase.verifyEqual(reg.count(), 0, 'empty_count');
             testCase.verifyTrue(isempty(reg.keys()), 'empty_keys');
         end
+
+        function testRegisterAndGet(testCase)
+            reg = ExternalSensorRegistry('TestLab');
+            s = Sensor('temp', 'Name', 'Temperature');
+            reg.register('temp', s);
+            out = reg.get('temp');
+            testCase.verifyEqual(out.Key, 'temp', 'get_key');
+            testCase.verifyEqual(out.Name, 'Temperature', 'get_name');
+            testCase.verifyEqual(reg.count(), 1, 'count_after_register');
+        end
+
+        function testGetUnknownKeyThrows(testCase)
+            reg = ExternalSensorRegistry('TestLab');
+            threw = false;
+            try
+                reg.get('nonexistent');
+            catch
+                threw = true;
+            end
+            testCase.verifyTrue(threw, 'should_throw');
+        end
+
+        function testUnregister(testCase)
+            reg = ExternalSensorRegistry('TestLab');
+            reg.register('temp', Sensor('temp'));
+            reg.unregister('temp');
+            testCase.verifyEqual(reg.count(), 0, 'empty_after_unregister');
+        end
+
+        function testGetMultiple(testCase)
+            reg = ExternalSensorRegistry('TestLab');
+            reg.register('a', Sensor('a'));
+            reg.register('b', Sensor('b'));
+            out = reg.getMultiple({'a', 'b'});
+            testCase.verifyEqual(numel(out), 2, 'getMultiple_count');
+            testCase.verifyEqual(out{1}.Key, 'a', 'getMultiple_key1');
+            testCase.verifyEqual(out{2}.Key, 'b', 'getMultiple_key2');
+        end
+
+        function testGetAll(testCase)
+            reg = ExternalSensorRegistry('TestLab');
+            reg.register('a', Sensor('a'));
+            reg.register('b', Sensor('b'));
+            m = reg.getAll();
+            testCase.verifyTrue(isa(m, 'containers.Map'), 'getAll_type');
+            testCase.verifyEqual(m.Count, uint64(2), 'getAll_count');
+        end
+
+        function testGetAllReturnsCopy(testCase)
+            reg = ExternalSensorRegistry('TestLab');
+            reg.register('a', Sensor('a'));
+            m = reg.getAll();
+            m('injected') = Sensor('injected');
+            % Original registry should be unaffected
+            testCase.verifyEqual(reg.count(), 1, 'copy_not_mutated');
+        end
+
+        function testRegisterNonSensorThrows(testCase)
+            reg = ExternalSensorRegistry('TestLab');
+            threw = false;
+            try
+                reg.register('bad', struct('Key', 'bad'));
+            catch
+                threw = true;
+            end
+            testCase.verifyTrue(threw, 'should_throw_non_sensor');
+        end
+
+        function testUnregisterNonexistentNoError(testCase)
+            reg = ExternalSensorRegistry('TestLab');
+            reg.unregister('nonexistent');  % should not error
+        end
     end
 end
